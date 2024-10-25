43942111068c74368b5e0609364eed40e4b9b4e6
Theofilo Henrique <151052072+TheofiloHenrique@users.noreply.github.com>
2024-10-24 23:23:10 -0300

    Initial commit

---
+++ src/test/java/br/com/naveguard/naveguardBackend/NaveguardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.naveguard.naveguardBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaveguardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
