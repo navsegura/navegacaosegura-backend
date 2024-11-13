a63b16d0c651e1f6f744020f2ec6b322a14888af
Guilherme <guilherme.1009@hotmail.com>
2024-11-13 16:58:41 -0300

    exceptions controller and exception handler

---
+++ src/main/java/br/com/naveguard/naveguardBackend/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,44 @@
+package br.com.naveguard.naveguardBackend.controllers.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+import br.com.naveguard.naveguardBackend.services.exceptions.DatabaseException;
+import br.com.naveguard.naveguardBackend.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+@ControllerAdvice
+public class ControllerExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError error = new StandardError(Instant.now(), status.value(), e.getMessage(), "Recurso não encontrado", request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError error = new StandardError(Instant.now(), status.value(), e.getMessage(), "Violação do banco de dados", request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> databaseException(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError error = new ValidationError(Instant.now(), status.value(), "Erro de Validação", e.getMessage(), request.getRequestURI());
+		for(FieldError f : e.getBindingResult().getFieldErrors()) {
+			error.addError(f.getField(), f.getDefaultMessage());
+		}
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	
+}
